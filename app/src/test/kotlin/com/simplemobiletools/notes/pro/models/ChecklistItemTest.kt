10a4918d2672fcc019abcdc46fbe01f1b39d5575
Dejvino <scoutmaster@dejvino.cz>
2023-07-31 04:46:16 +0200

    Parameterized test of compare to

--- app/src/test/kotlin/com/simplemobiletools/notes/pro/models/ChecklistItemTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/notes/pro/models/ChecklistItemTest.kt
@@ -1,16 +1,45 @@
 package com.simplemobiletools.notes.pro.models
 
-import org.junit.Test
+import com.simplemobiletools.commons.helpers.SORT_BY_TITLE
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ChecklistItemTest {
+@RunWith(Parameterized::class)
+class ChecklistItemTest(val sorting: Int, val itemOne: ChecklistItem, val expectedCompareTo: Int, val itemTwo: ChecklistItem) {
+
+    @Before
+    fun setup() {
+        ChecklistItem.sorting = sorting
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testCompareTo() {
+        assertEquals(expectedCompareTo, itemOne.compareTo(itemTwo))
+    }
+
+    companion object {
+        const val BEFORE = -1
+        const val AFTER = 1
+        const val EQUALS = 0
+
+        @JvmStatic
+        @Parameters()
+        fun data(): List<Array<Any>> {
+            return listOf(
+                arrayOf(SORT_BY_TITLE,
+                    ChecklistItem(1, 0, "A", false),
+                    BEFORE, ChecklistItem(2, 0, "B", false)),
+            )
+        }
     }
 }
