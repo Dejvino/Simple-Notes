0177c4ea0ebd2a22f0a0d98a956b8b61899ad302
Dejvino <scoutmaster@dejvino.cz>
2023-08-01 06:40:57 +0200

    Refactor CollatorBasedComparator

--- app/src/main/kotlin/com/simplemobiletools/notes/pro/helpers/CollatorBasedComparator.kt
+++ app/src/main/kotlin/com/simplemobiletools/notes/pro/helpers/CollatorBasedComparator.kt
@@ -2,83 +2,73 @@ package com.simplemobiletools.notes.pro.helpers
 
 import java.text.Collator
 
-// adapted from AlphanumericComparator
+/**
+ * Collator-based string comparator
+ *
+ * Adapted from AlphanumericComparator to support numerical collation.
+ */
 class CollatorBasedComparator: Comparator<String> {
     override fun compare(string1: String, string2: String): Int {
         val collator = getCollator()
 
         var thisMarker = 0
         var thatMarker = 0
-        val s1Length = string1.length
-        val s2Length = string2.length
 
-        while (thisMarker < s1Length && thatMarker < s2Length) {
-            val thisChunk = getChunk(string1, s1Length, thisMarker)
+        while (thisMarker < string1.length && thatMarker < string2.length) {
+            val thisChunk = getChunk(string1, string1.length, thisMarker)
             thisMarker += thisChunk.length
 
-            val thatChunk = getChunk(string2, s2Length, thatMarker)
+            val thatChunk = getChunk(string2, string2.length, thatMarker)
             thatMarker += thatChunk.length
 
-            // If both chunks contain numeric characters, sort them numerically.
-            var result: Int
-            if (isDigit(thisChunk[0]) && isDigit(thatChunk[0])) {
-                // Simple chunk comparison by length.
-                val thisChunkLength = thisChunk.length
-                result = thisChunkLength - thatChunk.length
-                // If equal, the first different number counts.
-                if (result == 0) {
-                    for (i in 0 until thisChunkLength) {
-                        result = thisChunk[i] - thatChunk[i]
-                        if (result != 0) {
-                            return coerce(result)
-                        }
-                    }
-                }
+            val result = if (isDigit(thisChunk[0]) && isDigit(thatChunk[0])) {
+                collateNumerically(thisChunk, thatChunk)
             } else {
-                result = collator.compare(thisChunk, thatChunk)
+                collator.compare(thisChunk, thatChunk)
             }
 
             if (result != 0) {
-                return coerce(result)
+                return coerceResult(result)
             }
         }
 
-        return coerce(s1Length - s2Length)
+        return coerceResult(string1.length - string2.length)
+    }
+
+    private fun collateNumerically(string1: String, string2: String): Int {
+        var result: Int
+        result = string1.length - string2.length
+        if (result == 0) {
+            // equal length, the first different number counts
+            for (i in string1.indices) {
+                result = string1[i] - string2[i]
+                if (result != 0) {
+                    break
+                }
+            }
+        }
+        return result
     }
 
     private fun getChunk(string: String, length: Int, marker: Int): String {
         var current = marker
-        val chunk = StringBuilder()
         var c = string[current]
-        chunk.append(c)
+        val chunk = StringBuilder(c.toString())
         current++
-        if (isDigit(c)) {
-            while (current < length) {
-                c = string[current]
-                if (!isDigit(c)) {
-                    break
-                }
-                chunk.append(c)
-                current++
+        val chunkOfDigits = isDigit(c)
+        while (current < length) {
+            c = string[current]
+            if (isDigit(c) != chunkOfDigits) {
+                break
             }
-        } else {
-            while (current < length) {
-                c = string[current]
-                if (isDigit(c)) {
-                    break
-                }
-                chunk.append(c)
-                current++
-            }
+            chunk.append(c)
+            current++
         }
         return chunk.toString()
     }
 
     private fun isDigit(ch: Char) = ch in '0'..'9'
-
-    private fun coerce(compareToResult: Int): Int {
-        return compareToResult.coerceIn(-1, 1)
-    }
+    private fun coerceResult(compareToResult: Int) = compareToResult.coerceIn(-1, 1)
 
     private fun getCollator(): Collator {
         val collator = Collator.getInstance()
